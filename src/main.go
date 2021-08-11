2a02ff1d45fd141be92bae5152a6ce38559adecd
Yanislav Igonin <yanislavdabest@gmail.com>
2021-08-11 17:52:15 +0300

    add view and edit pages

--- src/main.go
+++ src/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -16,7 +18,7 @@ type Page struct {
 
 func (p *Page) save() error {
 	filename := p.Title + ".txt"
-	return ioutil.WriteFile("../pages/"+filename, p.Body, 0600)
+	return ioutil.WriteFile(PAGES_DIR_PATH+filename, p.Body, 0600)
 }
 
 func loadPage(title string) (*Page, error) {
@@ -29,25 +31,43 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
-func createDir(path string) error {
-	return os.MkdirAll(path, os.ModePerm)
+func createDir() error {
+	return os.MkdirAll(PAGES_DIR_PATH, os.ModePerm)
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Fprintf(w, "<h1>Error</h1><div>%s</div>", err.Error())
+		return
+	}
+
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		"<form action=\"/save/%s\" method=\"POST\">"+
+		"<textarea name=\"body\">%s</textarea><br>"+
+		"<input type=\"submit\" value=\"Save\">"+
+		"</form>",
+		p.Title, p.Title, p.Body)
 }
 
 func main() {
-	err := createDir(PAGES_DIR_PATH)
+	err := createDir()
 	if err != nil {
 		panic(err)
 	}
 
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	err = p1.save()
-	if err != nil {
-		panic(err)
-	}
-
-	p2, err := loadPage("TestPage")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(string(p2.Body))
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	// http.HandleFunc("/save/", saveHandler)
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
